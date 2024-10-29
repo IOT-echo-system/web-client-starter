92e0739fcc029b221411ec68a2e938b48db9cf6c
Shivam Rajput <raj.shiviraj@gmail.com>
2024-10-29 10:25:21 +0530

    Shivi | Removed web api filter unnecessary.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ publishing {
             from(components["java"])
             groupId = "com.robotutor"
             artifactId = "web-client-starter"
-            version = "1.0.1"
+            version = "1.0.2"
 
             pom {
                 name.set("Reactive Logging Starter")
@@ -43,13 +43,17 @@ java {
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://maven.pkg.github.com/IOT-echo-system/web-client-starter")
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-            password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+    fun githubMavenRepository(name: String) {
+        maven {
+            url = uri("https://maven.pkg.github.com/IOT-echo-system/$name")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+            }
         }
     }
+
+    githubMavenRepository("logging-starter")
 }
 
 dependencies {
@@ -58,7 +62,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.google.code.gson:gson:2.8.9")
-    implementation("com.robotutor:logging-starter:1.0.2")
+    implementation("com.robotutor:logging-starter:1.0.0")
 }
 
 kotlin {
--- src/main/kotlin/com/robotutor/iot/UtilsConfiguration.kt
+++ src/main/kotlin/com/robotutor/iot/UtilsConfiguration.kt
@@ -1,14 +1,12 @@
 package com.robotutor.iot
 
 import com.mongodb.ConnectionString
-import com.robotutor.iot.filters.WebApiFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.core.env.Environment
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
-import org.springframework.web.server.WebFilter
 
 @Configuration
 class UtilsConfiguration(private val environment: Environment) {
@@ -18,10 +16,5 @@ class UtilsConfiguration(private val environment: Environment) {
     fun reactiveMongoTemplate(): ReactiveMongoTemplate {
         val uri = environment.getRequiredProperty("spring.data.mongodb.uri")
         return ReactiveMongoTemplate(SimpleReactiveMongoDatabaseFactory(ConnectionString(uri)))
-    }
-
-    @Bean
-    fun customApiFilter(): WebFilter {
-        return WebApiFilter()
     }
 }
